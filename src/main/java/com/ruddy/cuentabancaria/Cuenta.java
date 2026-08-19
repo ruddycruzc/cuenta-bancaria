f23aaefe1bbc9cf12f4248520a55f48cace3d9f1
Ruddy <ruddy391@gmail.com>
2026-08-19 12:00:15 +0200

    feat: add constructor to Cuenta class for initializing account attributes

--- src/main/java/com/ruddy/cuentabancaria/Cuenta.java
+++ src/main/java/com/ruddy/cuentabancaria/Cuenta.java
@@ -6,4 +6,13 @@ public class Cuenta {
     protected int numeroRetiros;
     protected float tasaAnual;
     protected float comisionMensual;
+
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+        this.numeroConsignaciones = 0;
+        this.numeroRetiros = 0;
+        this.comisionMensual = 0;
+    }
+
 }
