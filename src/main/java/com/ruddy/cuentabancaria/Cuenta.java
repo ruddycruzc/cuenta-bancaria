59592393747fd18f7ad7f01fef8cf350e45a5787
Ruddy <ruddy391@gmail.com>
2026-08-19 11:52:21 +0200

    feat: create Cuenta class with account attributes

---
+++ src/main/java/com/ruddy/cuentabancaria/Cuenta.java
@@ -0,0 +1,9 @@
+package com.ruddy.cuentabancaria;
+
+public class Cuenta {
+    protected float saldo;
+    protected int numeroConsignaciones;
+    protected int numeroRetiros;
+    protected float tasaAnual;
+    protected float comisionMensual;
+}
